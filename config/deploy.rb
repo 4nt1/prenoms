232b75f2cca9bbde5a00446e6969670890c7129f
4nt1 <antoinemary@hotmail.fr>
2015-11-12 21:00:39 +0100

    meh

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.4.0'
 set :application, 'prenoms'
 set :repo_url, 'git@github.com:4nt1/prenoms.git'
-set :deploy_to, '/home/deploy/lavatory'
+set :deploy_to, '/home/deploy/prenoms'
 set :pm2_config, 'config/pm2.json'
 set :linked_dirs, %w(
   node_modules
